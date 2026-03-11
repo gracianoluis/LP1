759260e34f8d8b444dcdf2dfdb084576034f6363
Graciano Luis <graciano.luisgustavo@gmail.com>
2026-03-11 16:03:12 -0300

    Organizando pastas e adicionando exercicios

---
+++ Exercicios_LP1/src/Exercicio02.java
@@ -0,0 +1,6 @@
+public class Exercicio02 {
+    static void main(String[] args) {
+        System.out.println("Olá");
+        System.out.println("Mundo");
+    }
+}
---
+++ Exercicios_LP1/src/Exercicio03.java
@@ -0,0 +1,5 @@
+public class Exercicio03 {
+    static void main(String[] args) {
+        System.out.println("Olá\nMundo");
+    }
+}
